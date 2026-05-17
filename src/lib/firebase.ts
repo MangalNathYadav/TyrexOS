ff70b85b8641467ba1751d8808227b8d0ad2f19c
MangalNathYadav <mangalynath54@gmail.com>
2026-05-17 14:27:33 +0530

    security: migrate Firebase configuration details to environment variables and add gitignored env.local config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXTr2srajicaga2Cta-2E47mjawMUSvAs",
-  authDomain: "e-learning-ac62a.firebaseapp.com",
-  databaseURL: "https://e-learning-ac62a.firebaseio.com",
-  projectId: "e-learning-ac62a",
-  storageBucket: "e-learning-ac62a.firebasestorage.app",
-  messagingSenderId: "208704804421",
-  appId: "1:208704804421:web:d161347f8d725d5ee8352c"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
